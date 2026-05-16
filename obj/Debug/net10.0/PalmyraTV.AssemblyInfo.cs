00a9e030d3653af23b9eef451af4bee06dd598ec
Isravel <isravelkanagarajd@gmail.com>
2026-05-16 23:08:15 +0530

    update

--- obj/Debug/net10.0/PalmyraTV.AssemblyInfo.cs
+++ obj/Debug/net10.0/PalmyraTV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalmyraTV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b018baa9b407f1f8f1e9b0911274d9e3b526f468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae5e254b3cac2ad2706220227621c98f01315bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalmyraTV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalmyraTV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
